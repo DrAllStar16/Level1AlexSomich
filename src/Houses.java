79763d63588664361546d65eee55f166c5296af5
DrAllStar16 <league@WTS20.attlocal.net>
2017-09-11 16:57:11 -0700

    drums

--- src/Houses.java
+++ src/Houses.java
@@ -13,22 +13,46 @@ public class Houses {
 		Tortoise.show();
 		Tortoise.setSpeed(10);
 		Tortoise.setPenWidth(5);
-		for (int i = 0; i < 11; i++) {
+		
+		for (int i = 0; i < 12; i++) {
+		int b=	r.nextInt(2);
+			if (b==0) {
 			housey();
+			}
+			else {
+				houseyp();
+			}
+		
 		}
 		
 	}
 
 	private static void housey() {
-		int h=r.nextInt(400)+40;
+		int h=r.nextInt(300)+40;
 		Tortoise.setPenColor(PenColors.getRandomColor());
-		Tortoise.move(100);
+		Tortoise.move(h);
 		Tortoise.turn(90);
 		Tortoise.move(25);
 		Tortoise.turn(90);
-		Tortoise.move(100);
+		Tortoise.move(h);
 		Tortoise.turn(-90);
 		Tortoise.setPenColor(Color.green);
 		Tortoise.move(15);
+		Tortoise.turn(-90);
+	}
+	private static void houseyp() {
+		int h=r.nextInt(300)+40;
+		Tortoise.setPenColor(PenColors.getRandomColor());
+		Tortoise.move(h);
+		Tortoise.turn(45);
+		Tortoise.move(25);
+		Tortoise.turn(90);
+		Tortoise.move(25);
+		Tortoise.turn(45);
+		Tortoise.move(h);
+		Tortoise.turn(-90);
+		Tortoise.setPenColor(Color.green);
+		Tortoise.move(15);
+		Tortoise.turn(-90);
 	}
 }
