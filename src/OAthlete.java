5776538eae9e3fb5fe7ad9c842daa57feb4252d6
DrAllStar16 <league@iMac.attlocal.net>
2018-03-05 16:56:33 -0800

    OAthelete IP, Olympics IP

--- src/OAthlete.java
+++ src/OAthlete.java
@@ -1,4 +1,30 @@
 
 public class OAthlete {
+	private String name;
+	private String country;
+	private String sport;
+	private int age;
+	private int Gold;
+	private int Silver;
+	private int Bronze;
 
+	void setGold(int num) {
+		this.Gold = num;
+	}
+
+	void setSilver(int num) {
+		this.Silver = num;
+	}
+
+	void setBronze(int num) {
+		this.Bronze = num;
+	}
+
+	public OAthlete(String Name, String Sport, String Country, int Age) {
+		this.name = Name;
+		this.country = Country;
+		this.sport = Sport;
+		this.age = Age;
+
+	}
 }
--- src/Olympics.java
+++ src/Olympics.java
@@ -1,4 +1,14 @@
 
 public class Olympics {
 
+public static void main(String[] args) {
+	OAthlete a =new OAthlete("BillyBobJoe","Basketball","Great Britin",21);
+	OAthlete b =new OAthlete("Pickle","Golf","Spain",99);
+	OAthlete c =new OAthlete("Spongebob","Surfing","Austrailea",13);
+	a.setSilver(2);
+	a.setBronze(1);
+	b.setSilver(1);
+	b.setBronze(2);
+	c.setGold(3);
+}
 }
