117ad9a478426a403a17d1b1a7225a7a34a80ec5
League Student <league@iMac-4.attlocal.net>
2018-03-26 16:57:49 -0700

    WackAMole, Color Buttons IP

---
+++ src/ColorButton.java
@@ -0,0 +1,12 @@
+
+public class ColorButton {
+public static void main(String[] args) {
+	new ColorButton();                                       
+}
+
+ColorButton(){
+	
+	
+	
+}
+}
--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,42 +1,79 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WhackAMole {
-	 JPanel p = new JPanel();
-	 Random r = new Random();
-	 public static void main(String[] args) {
-	WhackAMole WAM= new WhackAMole();
+public class WhackAMole implements ActionListener {
+	JPanel p = new JPanel();
+	Random r = new Random();
+	JFrame f = new JFrame();
+	static int z = 0;
+	static Date timeAtStart = new Date();
+	public static void main(String[] args) {
+		WhackAMole WAM = new WhackAMole();
 
+	}
 
-}
-
-WhackAMole(){
-	 JFrame f = new JFrame();
-	int y= r.nextInt(24);
-	 drawButtons(y);
+	WhackAMole() {
+		JFrame f = new JFrame();
+		int y = r.nextInt(24);
+		drawButtons(y);
 		f.add(p);
 		f.setVisible(true);
 		f.setSize(300, 300);
-	
 
-
-}
-void drawButtons(int n) {
-	
-	for (int i = 0; i < 24; i++) {
-		JButton b = new JButton();
-		b.setOpaque(true);
-		if (i==n) {
-			b.setText("MOLE!!");
-
-		}
-		
-		p.add(b);
 	}
-	
-	
-}
+
+	void drawButtons(int n) {
+
+		for (int i = 0; i < 24; i++) {
+			JButton b = new JButton();
+			b.addActionListener(this);
+			b.setOpaque(true);
+			if (i == n) {
+				b.setText("MOLE");
+
+			}
+			f.add(p);
+			p.add(b);
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton button = (JButton) e.getSource();
+		if (button.getText().equals("MOLE")) {
+			z++;
+			speak("Correct");
+			if (z==10) {
+			endGame(timeAtStart, 10);
+			}
+			f.dispose();
+			WhackAMole WAM = new WhackAMole();
+		
+		}
+		else {
+			speak("You SUCK");  
+			
+		}
+	}
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+		+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+		}
+
 }
